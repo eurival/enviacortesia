7757097f86e0f1bf47971446c35e79ba08727a47
Eurival Bernardino da S.Gomes <eurival_b@hotmail.com>
2025-07-13 03:28:37 -0300

    Corrige fontes2

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,6 @@
-#mvn clean package
+mvn clean package
 docker login
-#docker build -t cortesia-processor-service .
-#docker tag cortesia-processor-service:latest eurival/cortesia-processor-service:latest 
-#docker push eurival/cortesia-processor-service:latest  
-./mvnw -DskipTests jib:build 
+docker build -t cortesia-processor-service .
+docker tag cortesia-processor-service:latest eurival/cortesia-processor-service:latest 
+docker push eurival/cortesia-processor-service:latest  
 echo "Processo concluido"
